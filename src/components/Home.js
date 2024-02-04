e17f4e6b7ecdfc9f30480c6313be606ab6d64619
dartgowtham <142606664+dartgowtham@users.noreply.github.com>
2024-02-04 20:24:09 +0530

    Update Home.js

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,9 +1,9 @@
-import React from 'react'
+ React from 'react'
 
 export default function Home() {
   return (
     <div style={{backgroundColor:'white',height:'100vh',width:'195vh',padding:0,margin:0}}>
-        <h1>This is Home</h1>
+        //<h1>This is Home</h1>
     </div>
   )
 }
